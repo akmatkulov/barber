ee641bd5c797a8dbbe180ac7e0f29ae0e6af77c6
sovetbek <akmatkulovs991@gmail.com>
2022-01-06 18:58:42 +0300

    Add views and update app.rb

--- app.rb
+++ app.rb
@@ -7,3 +7,23 @@ require 'sqlite3'
 get '/' do
 	erb "Hello!"
 end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+	erb :visit
+end
+
+get '/contacts' do
+	erb :contacts
+end
+
+post '/contacts' do
+	erb :contacts
+end
+
+get '/about' do
+	erb :about
+end
