8475ff1b08a63007ac2f388c6271559cecb8beb6
sovetbek <akmatkulovs991@gmail.com>
2022-01-08 19:02:51 +0300

    Include SQLite3

--- app.rb
+++ app.rb
@@ -4,6 +4,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def get_db
+	return SQLite3::Database.new 'barbershop.db'
+end
+
+# Initial database
+configure do
+
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
+				"Users"
+				(
+					"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+					"user_name" TEXT,
+					"user_phone" TEXT,
+					"date_stamp" TEXT,
+					"barber" TEXT,
+					"color" TEXT
+					)'
+end
+
 get '/' do
 	erb "Hello!"
 end
@@ -13,7 +33,32 @@ get '/visit' do
 end
 
 post '/visit' do
-	erb :visit
+
+	@user_name = params[:user_name]
+	@user_phone = params[:user_phone]
+	@date_time = params[:date_time]
+	@barber = params[:barber]
+	@color = params[:user_color]
+
+	# Validation
+	hh = { :user_name => "Введите имя",
+				 :user_phone => "Введите номер телефона",
+			 	 :date_time => "Введите дату и время" }
+
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+
+	if @error != ''
+		return erb :visit
+	end
+
+	db = get_db
+	db.execute 'insert into Users (user_name, user_phone, date_stamp, barber, color)
+							values (?, ?, ?, ?, ?)', [@user_name, @user_phone, @date_time, @barber, @color]
+
+	@title = "Thank you!"
+	@message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time}. #{@color}."
+
+	erb :message
 end
 
 get '/contacts' do
