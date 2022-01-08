63677874fc6dbca962d7ffad925523bb168cb731
sovetbek <akmatkulovs991@gmail.com>
2022-01-08 21:52:13 +0300

    Init Barber table on configure

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barber where barber_name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+	barbers.each do |barber|
+		if ! is_barber_exists? db, barber
+			db.execute 'insert into Barber (barber_name) values (?)', [barber]
+		end
+	end
+end
+
 def get_db
 	db = SQLite3::Database.new 'barbershop.db'
 	db.results_as_hash = true
@@ -30,10 +42,11 @@ configure do
 				(
 					"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 					"barber_name" TEXT
+				)'
 
-					)'
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 
-end
+	end
 
 get '/' do
 	erb "Hello!"
